00d631d7b01cb2dcc2a7cc4d1f8e4fd3a102fb54
sujangurung <sujangurung@sujangurungsmbp.home>
2022-05-18 18:17:55 +0100

    components Added

--- wrestling_show/src/main/java/com/example/wrestling_show/components/DataLoader.java
+++ wrestling_show/src/main/java/com/example/wrestling_show/components/DataLoader.java
@@ -1,4 +1,30 @@
 package com.example.wrestling_show.components;
 
-public class DataLoader {
+import com.example.wrestling_show.models.Wrestler;
+import com.example.wrestling_show.repositories.AudienceRepository;
+import com.example.wrestling_show.repositories.PurchaseRepository;
+import com.example.wrestling_show.repositories.ShowRepository;
+import com.example.wrestling_show.repositories.WrestlerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+
+public class DataLoader implements ApplicationRunner {
+
+    @Autowired
+    private PurchaseRepository purchaseRepository;
+
+    @Autowired
+    private WrestlerRepository wrestlerRepository;
+
+    @Autowired
+    private ShowRepository showRepository;
+
+    @Autowired
+    private AudienceRepository audienceRepository;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+    }
 }
