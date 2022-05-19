5a1e1fe271433b83d029b0326ee1635aacb8e071
sujangurung <sujangurung@sujangurungsmbp.home>
2022-05-19 16:40:39 +0100

    Audience and Shoe Controller Added

--- wrestling_show/src/main/java/com/example/wrestling_show/controllers/ShowController.java
+++ wrestling_show/src/main/java/com/example/wrestling_show/controllers/ShowController.java
@@ -1,6 +1,42 @@
 package com.example.wrestling_show.controllers;
 
 
+import com.example.wrestling_show.models.Audience;
+import com.example.wrestling_show.models.Show;
+import com.example.wrestling_show.repositories.AudienceRepository;
+import com.example.wrestling_show.repositories.ShowRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-public class ShowController  {
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("shows")
+public class ShowController {
+    @Autowired
+    ShowRepository showRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Show>> getshows() {
+        return new ResponseEntity<>(showRepository.findAll(), HttpStatus.OK);
+    }
+
+    //Show Mapping
+
+
+    //Show
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Show>> getshow(@PathVariable Long id) {
+        return new ResponseEntity<>(showRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Show> createShow(@RequestBody Show newShow) {
+        showRepository.save(newShow);
+        return new ResponseEntity<>(newShow, HttpStatus.CREATED);
+    }
 }
+
--- wrestling_show/src/main/java/com/example/wrestling_show/repositories/ShowRepository.java
+++ wrestling_show/src/main/java/com/example/wrestling_show/repositories/ShowRepository.java
@@ -4,6 +4,9 @@ import com.example.wrestling_show.models.Show;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ShowRepository extends JpaRepository<Show, Long> {
+//    public List<Show> getShowsByLocation();
 }
